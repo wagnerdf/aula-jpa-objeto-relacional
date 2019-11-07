6b70fd24ad0077d7c843d5d0f8d042163d70a7ad
Wagner Andrade <wagnerandrade_df@hotmail.com>
2019-11-07 18:06:31 -0300

    Buscando objeto pelo ID

--- src/aplicacao/Program.java
+++ src/aplicacao/Program.java
@@ -7,23 +7,23 @@ import javax.persistence.Persistence;
 import dominio.Pessoa;
 
 
+
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		System.out.println("INICIO");
 	
-		Pessoa p1 = new Pessoa(null,"Wagner Augusto de Andrade", "wagner@gmail.com");
-		Pessoa p2 = new Pessoa(null,"Marina Amorim Moura", "marina@gmail.com");
-		Pessoa p3 = new Pessoa(null,"Bruna da Silva", "bruna@gmail.com");
-		
+				
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();  	
+		
+		Pessoa p = em.find(Pessoa.class, 15);		
+		
+		System.out.println(p);
+		
+		
 		System.out.println("OK!");
 		
 		em.close();
